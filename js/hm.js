5d65883ceeccee92f0f075876de6159c60d157e7
acmic7 <xinxin_7899@163.com>
2023-02-10 10:37:28 +0800

    Site updated: 2023-02-10 10:37:13

---
+++ js/hm.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?28514a0eb8962bfe8470cab996bb7dc0";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
